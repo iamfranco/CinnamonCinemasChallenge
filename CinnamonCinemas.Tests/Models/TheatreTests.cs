c3c138f6bdf11477610dcec6301c72c319ff5a06
iamfranco <kinfaifrancochan@gmail.com>
2022-06-17 21:12:26 +0100

    add Theatre class with constructor, unit test constructor for null or empty input parameters

---
+++ CinnamonCinemas.Tests/Models/TheatreTests.cs
@@ -0,0 +1,53 @@
+﻿using CinnamonCinemas.Models;
+
+namespace CinnamonCinemas.Tests.Models;
+internal class TheatreTests
+{
+    Theatre theatre;
+    List<string> rowLetters;
+    List<int> columnNumbers;
+    [SetUp]
+    public void Setup()
+    {
+        rowLetters = new() { "A", "B", "C" };
+        columnNumbers = new() { 1, 2, 3, 4, 5 };
+        theatre = new Theatre(rowLetters, columnNumbers);
+    }
+
+    [Test]
+    public void Construct_With_Null_RowLetters_Should_Throw_Exception()
+    {
+        rowLetters = null;
+
+        Action act = () => theatre = new Theatre(rowLetters, columnNumbers);
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Test]
+    public void Construct_With_Empty_RowLetters_Should_Throw_Exception()
+    {
+        rowLetters = new() { };
+
+        Action act = () => theatre = new Theatre(rowLetters, columnNumbers);
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void Construct_With_Null_ColumnNumbers_Should_Throw_Exception()
+    {
+        columnNumbers = null;
+
+        Action act = () => theatre = new Theatre(rowLetters, columnNumbers);
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Test]
+    public void Construct_With_Empty_ColumnNumbers_Should_Throw_Exception()
+    {
+        columnNumbers = new() { };
+
+        Action act = () => theatre = new Theatre(rowLetters, columnNumbers);
+        act.Should().Throw<ArgumentException>();
+    }
+
+}
---
+++ CinnamonCinemas/Models/Theatre.cs
@@ -0,0 +1,18 @@
+﻿namespace CinnamonCinemas.Models;
+public class Theatre
+{
+    public Theatre(List<string> rowLetters, List<int> columnNumbers)
+    {
+        if (rowLetters is null) 
+            throw new ArgumentNullException(nameof(rowLetters));
+
+        if (rowLetters.Count == 0)
+            throw new ArgumentException($"{nameof(rowLetters)} cannot be empty");
+
+        if (columnNumbers is null)
+            throw new ArgumentNullException(nameof(columnNumbers));
+
+        if (columnNumbers.Count == 0)
+            throw new ArgumentException($"{nameof(columnNumbers)} cannot be empty");
+    }
+}

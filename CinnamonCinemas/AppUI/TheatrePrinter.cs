63b470ca7c86b618fff0fa9ccf864508000d3b66
iamfranco <kinfaifrancochan@gmail.com>
2022-06-18 14:43:04 +0100

    add TheatrePrinter class and use in Program.cs

---
+++ CinnamonCinemas/AppUI/TheatrePrinter.cs
@@ -0,0 +1,47 @@
+﻿using CinnamonCinemas.Models;
+using Spectre.Console;
+using System.Collections.ObjectModel;
+
+namespace CinnamonCinemas.AppUI;
+public class TheatrePrinter
+{
+    public void Print(Theatre theatre) => Print(theatre, new List<Seat>().AsReadOnly());
+
+    public void Print(Theatre theatre, ReadOnlyCollection<Seat> recentlyAllocatedSeats)
+    {
+        var table = new Table();
+
+        table.AddColumns(Enumerable.Range(0, theatre.ColumnCount).Select(x => x.ToString()).ToArray());
+        table.HideHeaders();
+
+        Queue<Seat> seats = new(theatre.Seats);
+
+        for (int row = 0; row < theatre.RowCount; row++)
+        {
+            table.AddEmptyRow();
+            for (int column = 0; column < theatre.ColumnCount; column++)
+            {
+                Seat seat = seats.Dequeue();
+                string seatNumber = seat.SeatNumber;
+
+                if (seat.Status is Models.Status.Allocated)
+                    seatNumber = $"[red]{seat.SeatNumber}[/]";
+
+                if (recentlyAllocatedSeats.Contains(seat))
+                    seatNumber = $"[orange3]{seat.SeatNumber}[/]";
+
+                table.UpdateCell(row, column, seatNumber);
+            }
+        }
+
+        AnsiConsole.Write(table);
+
+        int totalSeatsCount = theatre.Seats.Count();
+        int availableSeatsCount = theatre.GetAvailableSeatsCount();
+
+        AnsiConsole.MarkupLine($"Available Seats : {availableSeatsCount}");
+        AnsiConsole.MarkupLine($"[red]Allocated[/] Seats : {totalSeatsCount - availableSeatsCount}");
+
+        Console.WriteLine();
+    }
+}
--- CinnamonCinemas/Program.cs
+++ CinnamonCinemas/Program.cs
@@ -1,24 +1,46 @@
-﻿using CinnamonCinemas.Models;
+﻿using CinnamonCinemas.AppUI;
+using CinnamonCinemas.Models;
 using CinnamonCinemas.Models.SeatNumberGenerators;
+using Spectre.Console;
 
 SeatNumberGenerator seatNumberGenerator = new SeatNumberGenerator();
 Theatre theatre = new Theatre(rowCount: 3, columnCount: 5, seatNumberGenerator);
+TheatrePrinter theatrePrinter = new TheatrePrinter();
 
+theatrePrinter.Print(theatre);
 while (true)
 {
-    Console.WriteLine($"Available Seats Count: {theatre.GetAvailableSeatsCount()}");
-    int numberOfSeats = new Random().Next(1, 4);
+    int availableSeatsCount = theatre.GetAvailableSeatsCount();
+
+    if (availableSeatsCount == 0)
+    {
+        Console.WriteLine("All seats are allocated, no more seats to sell.");
+        break;
+    }
+
+    int numberOfSeats = AnsiConsole.Prompt(
+        new TextPrompt<int>("Enter [blue]number of seats[/] to purchase (between 1 and 3): ")
+            .ValidationErrorMessage("[red]Number of seats[/] must be [blue]between 1 and 3[/].\n")
+            .Validate(x => x >= 1 && x <= 3)
+        );
+
     var allocatedSeats = theatre.AllocateSeats(numberOfSeats);
     
     if (allocatedSeats is null)
     {
-        Console.WriteLine($"Customer wanted {numberOfSeats} seats, but we don't have enough available seats.");
-        break;
+        string seatsString = availableSeatsCount > 1 ? "seats" : "seat";
+
+        AnsiConsole.MarkupLine($"Customer wanted [orange3]{numberOfSeats}[/] seats, " +
+            $"but we only have [red]{availableSeatsCount}[/] {seatsString} available.\n");
+        continue;
     }
 
-    var allocatedSeatNumbers = allocatedSeats.Select(seat => seat.SeatNumber).ToList();
-    Console.WriteLine($"Customer wants {numberOfSeats} seats, allocated at {string.Join(", ", allocatedSeatNumbers)}");
+    Console.Clear();
+    theatrePrinter.Print(theatre, allocatedSeats);
 
+    var allocatedSeatNumbers = allocatedSeats.Select(seat => seat.SeatNumber).ToList();
+    AnsiConsole.MarkupLine($"Customer purchased [orange3]{numberOfSeats}[/] seats, " +
+        $"allocated at [orange3]{string.Join(", ", allocatedSeatNumbers)}[/]\n");
 }
 
 Console.ReadLine();
